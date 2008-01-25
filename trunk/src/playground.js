4d9288faad84701a700b8eba844b0c10a726f3a9
knutbo <knutbo@07c8278f-7844-0410-a441-e74e8264e3ac>
2008-01-25 19:13:31 +0000

    Display of grid and some initial styling just for show.

--- trunk/src/playground.js
+++ trunk/src/playground.js
@@ -1,3 +1,31 @@
+function Arena(width, height) {
+
+    this.draw = function (target) {
+        var table = new Element('table', {'class':'arena'});
+
+        target.insert(table);
+
+        for (y = 0; y < this.height; y++) {
+            var row = new Element('tr');
+            table.insert(row);
+            for (x = 0; x < this.width; x++) {
+                var col = new Element('td');
+                row.insert(col);
+            }
+        }
+    }
+
+    this.width = width;
+    this.height = height;
+
+
+}
+
+
 function startBattle() {
-alert("Here we go!");
+    var arena = new Arena(20, 20);
+
+    arena.draw($('playground'));
+
+
 }
