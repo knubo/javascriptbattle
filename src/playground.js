54b3fd26b09eea3536be52dae4b83d937d6687e5
knutbo <knutbo@07c8278f-7844-0410-a441-e74e8264e3ac>
2008-01-30 22:57:33 +0000

    Laser

--- src/playground.js
+++ src/playground.js
@@ -41,13 +41,25 @@ function Arena(width, height) {
     this.newBot = function(bot) {
         var div = new Element('div', {'class':'robot'});
         var pic = new Element('img', {'class':'robotImg', 'src':bot.botBrain.picUrl, 'alt':'Player icon', 'title':'Player ' + bot.botBrain.name});
+        var boom = new Element('img', {'class': 'boom', 'src':'images/Explosion.gif'});
+        var radar = new Element('img', {'class': 'radar', 'src':'images/radar1.gif'});
+        var star = new Element('img', {'class':'starImg','src':'images/star.png'});
+        var laser = new Element('div', {'class':'laser'});
+
+        boom.setStyle("display:none");
+        radar.setStyle("display:none");
+
         bot.img = div;
+        bot.boom = boom;
+        bot.radar = radar;
+        bot.star = star;
+        bot.laser = laser;
 
         div.insert(pic);
-
-        var star = new Element('img', {'class':'starImg','src':'images/star.png'});
-        bot.star = star;
+        div.insert(radar);
+        div.insert(boom);
         div.insert(star);
+        div.insert(laser);
 
         this.turnRobot(bot);
         this.moveRobot(bot, -1, -1);
@@ -58,8 +70,7 @@ function Arena(width, height) {
     }
 
     this.explode = function(bot) {
-
-        var boom = $('boom');
+        var boom = bot.boom;
         bot.img.insert(boom);
         
         boom.setStyle("display:block");
@@ -68,13 +79,28 @@ function Arena(width, height) {
     }
 
     this.radar = function(bot) {
-        var radar = $('radar');
-        bot.img.insert(radar);
-
+        var radar = bot.radar;
         radar.setStyle("display:block");
 
         new Effect.Fade(radar);
     }
+
+    this.laser = function(bot) {
+        var laser = bot.laser;
+
+        if(bot.dir == Bot.DIRSOUTH) {
+            laser.setStyle("display:block; width:5;height:85;left:13;top:25");
+        } else if(bot.dir == Bot.DIRNORTH) {
+            laser.setStyle("display:block; width:5;height:90;left:13;top:-70");            
+        } else if(bot.dir == Bot.DIRWEST) {
+            laser.setStyle("display:block; width:85;height:5;left:-75;top:13");
+        } else if(bot.dir == Bot.DIREAST) {
+            laser.setStyle("display:block; width:90;height:5;left:20;top:13");
+        }
+        new Effect.Fade(laser, { duration: 0.5 });
+
+    }
+
 }
 
 
@@ -83,12 +109,6 @@ function startBattle() {
     var robots = new Robots(arena);
 
     arena.drawArena($('playground'));
-
-    var boom = new Element('img', {'class':'explosion', 'src':'images/Explosion.gif', 'id':'boom'});
-    var radar = new Element('img', {'class':'radar', 'src':'images/radar1.gif', 'id':'radar'});
-
-    $('playground').insert(boom);
-    $('playground').insert(radar);
 
     robots.addBot(new Bot(new BotBrain1(), 12, 3, Bot.DIRNORTH));
     robots.addBot(new Bot(new BotBrain2(), 10, 1, Bot.DIRSOUTH));
@@ -103,13 +123,13 @@ function startBattle() {
     var toWatch = robots.bots[0];
 
     toWatch.img.observe('click', function() {
-        arena.explode(toWatch);
+        arena.laser(toWatch);
     });
 
     var toWatch2 = robots.bots[1];
 
     toWatch2.img.observe('click', function() {
-        arena.explode(toWatch2);
+        arena.laser(toWatch2);
     });
 
 }    
