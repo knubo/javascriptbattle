5535eb7659a33b43fbb6d2607f76d6323ede5fda
klaus.stafto <klaus.stafto@07c8278f-7844-0410-a441-e74e8264e3ac>
2008-01-30 20:33:19 +0000

    Tester ulike retninger.

--- src/bot-examples/botBrain1.js
+++ src/bot-examples/botBrain1.js
@@ -1,15 +1,19 @@
 function BotBrain1() {
     this.name = 'Klaus';
     this.picUrl = 'images/kst.jpg';
+    this.count = 0;
 
 	//Should return a Command object.
 	this.decide = function(bot) {
 		var command = new Command();
 
-		if (bot.y > 0) {
+		if (count % 4 == 0) {
+            command.turn = Command.TURNLEFT;
+		} else {
 			command.move = Command.MOVEFRONT;
 		}
-			
+
+        count++;
 		return command;
 	}
 
