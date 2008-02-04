0d24bb2b12a0701eae628dc16d27da5ac64b23c0
knutbo <knutbo@07c8278f-7844-0410-a441-e74e8264e3ac>
2008-02-04 20:29:01 +0000

    Some bread slide + more keys for moving slides.

--- trunk/presentation/presentation.js
+++ trunk/presentation/presentation.js
@@ -1,7 +1,7 @@
 var currentSlide = 1;
 
 function loadSlide() {
-    new Ajax.Updater('presentationInner', "slides/slide"+currentSlide+".html");
+    new Ajax.Updater('presentationInner', "slides/slide" + currentSlide + ".html");
     $('pageNumber').innerHTML = currentSlide;
 }
 
@@ -14,7 +14,7 @@ function setup() {
 
     document['onkeypress'] = function(e) {
 
-        if(e.charCode == 32) {
+        if (e.charCode == 32 || e.keyCode == 39 || e.charCode == 110) {
             currentSlide++;
             loadSlide();
             doSomethingCool();
@@ -22,7 +22,7 @@ function setup() {
             return;
         }
 
-        if(e.keyCode == 8 || e.charCode == 112) {
+        if (currentSlide > 1 && (e.keyCode == 8 || e.charCode == 112 || e.keyCode == 37)) {
             currentSlide--;
             loadSlide();
             Event.stop(e);
