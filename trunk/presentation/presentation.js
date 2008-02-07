65153a06cacec78c5508838b55556152d5eac128
knutbo <knutbo@07c8278f-7844-0410-a441-e74e8264e3ac>
2008-02-07 19:09:29 +0000

    Quick jump (pretty cool - activate with c)

--- trunk/presentation/presentation.js
+++ trunk/presentation/presentation.js
@@ -1,7 +1,55 @@
+function PageSelect() {
+
+    this.loadSlide = function(slideNumber) {
+
+        var pageSelect = this;
+
+        new Ajax.Request('slides/slide' + slideNumber + ".html", {
+            onSuccess: function(transport) {
+                var matchResult = transport.responseText.match("@T:(.*)@");
+
+                if(matchResult && matchResult.length == 2) {
+                    pageSelect.addSlide(slideNumber, matchResult[1]);
+                }
+
+                if (!transport.responseText.match("@LastSlide@")) {
+                    pageSelect.loadSlide(slideNumber+1);
+                }
+
+            }
+        });
+    }
+
+    this.show = function() {
+        var options = $('pages').childElements();
+
+        if(!options || options.length == 0) {
+            this.loadSlide(1);
+        }
+    }
+
+    this.addSlide = function(slideNumber, title) {
+        var option = new Element('option', {'value': slideNumber});
+        option.innerHTML = slideNumber+" - "+title;
+
+        $('pages').insert(option);
+    }
+    
+}
+
+
 var currentSlide = 1;
 
 /* Define this and save a warning. Will be redefined and set by firebug. */
 var console;
+
+var pageSelect = new PageSelect();
+
+function viewSlide(slideNumber) {
+    currentSlide = slideNumber;
+    loadSlide();
+    $('pageselect').setStyle("display:none;");
+}
 
 function loadSlide() {
     new Ajax.Updater('presentationInner', "slides/slide" + currentSlide + ".html", {evalScripts:true});
@@ -12,7 +60,11 @@ function lastSlide() {
     return $('presentationInner').innerHTML.match("@LastSlide@");
 }
 
-function doSomethingCool() {
+
+function showPageselect() {
+    $('pageselect').setStyle("display:block");
+    pageSelect.show();
+    $('pages').focus();
 
 }
 
@@ -21,10 +73,15 @@ function setup() {
 
     document['onkeypress'] = function(e) {
 
+        if (e.charCode == 99) {
+            showPageselect();
+            Event.stop(e);
+            return;
+        }
+
         if (!lastSlide() && (e.charCode == 32 || e.keyCode == 39 || e.charCode == 110)) {
             currentSlide++;
             loadSlide();
-            doSomethingCool();
             Event.stop(e);
             return;
         }
@@ -36,7 +93,6 @@ function setup() {
             return;
         }
         console.log(e);
-
     }
 
 
