6cb4da0e7e5113d6d14da7c257be3ebea775a7d5
knutbo <knutbo@07c8278f-7844-0410-a441-e74e8264e3ac>
2008-02-24 22:20:31 +0000

    More nifty server side stuff for obtaining bots.

---
+++ server/ajax/bot.php
@@ -0,0 +1,21 @@
+<?php
+include_once ("../lib/AppConfig.php");
+include_once ("../lib/DB.php");
+include_once ("../lib/Brains.php");
+
+$name = array_key_exists("name", $_REQUEST) ? $_REQUEST["name"] : "";
+$action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : "list";
+
+$db = new DB();
+$brains = new Brains($db);
+
+switch ($action) {
+	case "get" :
+		echo $brains->getOne($name);
+		break;
+	case "list" :
+		echo json_encode($brains->listAll());
+		break;
+}
+?>
+  
